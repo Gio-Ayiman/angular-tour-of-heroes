bd6a0e84f4fe41917a375145a1b4375472c36251
Giovanni Ayimambenwe <gioayima@gmail.com>
2022-05-19 12:19:26 +0200

    Angular Tour of Heroes final

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,40 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
+
+  save(): void {
+    if(this.hero){
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack())
+    }
+  }
+
+}
